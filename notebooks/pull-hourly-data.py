f48d32c71b199357a061f1402bde3b3561baf0e9
abhishek_ku@yahoo.com <abhishek_ku@yahoo.com>
2022-06-27 16:36:12 +0000

    working notebook

--- notebooks/pull-hourly-data.py
+++ notebooks/pull-hourly-data.py
@@ -6,14 +6,19 @@
 ## Initilize token from the secret.
 #token = dbutils.secrets.get(scope="aktechthoughts", key="github-token") # Not required for next command.
 #pip install git+https://abhishek_ku@yahoo.com:$token@https://github.com/aktechthoughts/databricks-extract-load-dbt/blob/main/requirements.txt
-#%pip install -r https://raw.githubusercontent.com/aktechthoughts/databricks-extract-load-dbt/main/requirements.txt
 
+
+# COMMAND ----------
+
+## Install directly from the repository file.
+#%pip install -r https://raw.githubusercontent.com/aktechthoughts/databricks-extract-load-dbt/main/requirements.txt
 
 # COMMAND ----------
 
 ## Read data from remote api
 
-import urllib.request, json, pandas as pd
+import urllib.request, json,pandas as pd
+from datetime import datetime
 url = "https://start.vag.de/dm-poc/api/v2/fahrten"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
@@ -35,7 +40,11 @@ df['version'] = ver
 
 # Save data in parquet file.
 
-date_time = now.strftime("%m%d%Y%H%M%S")
+date_time = datetime.now().strftime("%Y%m%d%H%M%S")
 df.to_parquet('/dbfs/bronze/movement_'+date_time+'.gzip', compression='gzip')
 
 #
+
+# COMMAND ----------
+
+
