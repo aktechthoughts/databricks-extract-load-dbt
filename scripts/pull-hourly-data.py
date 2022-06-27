5ec455893b74ca89cc9b48316af3eff014d7ad8b
Abhishek Kumar <abhishek_ku@yahoo.com>
2022-06-27 13:47:24 +0100

    Added scripts.

---
+++ scripts/pull-hourly-data.py
@@ -0,0 +1,7 @@
+# %%
+#Notebook to pull data from API to raw parquet file.
+
+# %%
+##This notebook is executed every hour and pulls data from https://start.vag.de/dm-poc/api/v2/fahrten and store in parquet file.
+
+
