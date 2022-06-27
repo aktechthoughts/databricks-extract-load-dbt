2cc297e7be61c6bcdf9dfedfa3b54c1a6f8127fc
Abhishek Kumar <abhishek_ku@yahoo.com>
2022-06-27 15:15:44 +0100

    Added exÃport parquet.

--- notebooks/pull-hourly-data.py
+++ notebooks/pull-hourly-data.py
@@ -4,3 +4,19 @@
 # COMMAND ----------
 
 ##
+
+import urllib.request, json, pandas as pd
+url = "https://start.vag.de/dm-poc/api/v2/fahrten"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+ts = result['metadata']['timestamp']
+ver = result['metadata']['version']
+
+df = pd.json_normalize(result['data'])
+df['ts'] = ts
+df['version'] = ver
+
+df.to_parquet('df.parquet.gzip', compression='gzip')
+
+df1=pd.read_parquet('df.parquet.gzip')
+print(df1)
