60d3916e22ada8bd77f91073b44c73b2a440e255
abhishek_ku@yahoo.com <abhishek_ku@yahoo.com>
2022-06-27 15:43:04 +0000

    added load to dbfs

--- notebooks/pull-hourly-data.py
+++ notebooks/pull-hourly-data.py
@@ -3,12 +3,18 @@
 
 # COMMAND ----------
 
-##
+## Read data from remote api
 
 import urllib.request, json, pandas as pd
 url = "https://start.vag.de/dm-poc/api/v2/fahrten"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
+
+
+
+# COMMAND ----------
+
+# Save data in local variable.
 ts = result['metadata']['timestamp']
 ver = result['metadata']['version']
 
@@ -16,6 +22,10 @@ df = pd.json_normalize(result['data'])
 df['ts'] = ts
 df['version'] = ver
 
-date_time = now.strftime("%m%d%Y%H%M%S")
-df.to_parquet('movement_'+date_time+'.gzip', compression='gzip')
 
+# COMMAND ----------
+
+# Save data in parquet file.
+
+date_time = now.strftime("%m%d%Y%H%M%S")
+df.to_parquet('/dbfs/bronze/movement_'+date_time+'.gzip', compression='gzip')
