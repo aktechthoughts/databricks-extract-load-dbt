76817d4248c63a1faba432c76f7c0bafd75d3385
abhishek_ku@yahoo.com <abhishek_ku@yahoo.com>
2022-06-27 12:20:24 +0000

    updated
    Changed from databricks console.

---
+++ scripts/pull-hourly-data.ipynb.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+#Notebook to pull data from API to raw parquet file.
+
+# COMMAND ----------
+
+##This notebook is executed every hour and pulls data from https://start.vag.de/dm-poc/api/v2/fahrten and store in parquet file.
