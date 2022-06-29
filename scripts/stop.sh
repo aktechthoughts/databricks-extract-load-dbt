c66165cb5db7b30dc7a9b8b5dd88cc7fca4ad8a3
Abhishek Kumar <abhishek_ku@yahoo.com>
2022-06-29 21:10:10 +0100

    Added control to handle finished jobs.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,3 +2,4 @@
 #set -x
 cluster_id=$(databricks clusters list | grep RUNNING | awk '{ print $1}')
 databricks clusters delete --cluster-id $cluster_id > /dev/null 2>&1 
+echo "The clusrer id:"$cluster_id" is stopped."
