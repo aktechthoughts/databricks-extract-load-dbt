01b07604ecf2759ffa4c2f6f8b270ffbff21c575
Abhishek Kumar <abhishek_ku@yahoo.com>
2022-07-10 19:08:25 +0100

    This contains mart or report.

---
+++ models/gold/movement_report.sql
@@ -0,0 +1,23 @@
+with daily_movement as (
+    select 
+        journey_number,
+        bus_number,
+        line_number,
+        line_name,
+        product,
+        direction,
+        destination,
+        start_time,
+        end_time,
+        journey_finished,
+        route,
+        received_time_stamp,
+        version_of_message
+    from
+       {{ source('silver', 'movement') }}
+)
+select
+    journey_number,
+    bus_number
+from 
+    daily_movement
